bb9e450783230d7a7794eea5f844770af53cc71d
jchen8tw <11806483+jchen8tw@users.noreply.github.com>
2020-08-14 09:49:13 +0800

    fix installChaincode.sh

--- single/installChaincode.sh
+++ single/installChaincode.sh
@@ -6,14 +6,13 @@ CHAINCODE_PACKAGE_NAME=$1
 CHAINCODE_NAME=$2
 SEQUENCE=$3
 VERSION=$4
-COLLECTION_PROFILE_PATH=chaincode/$(CHAINCODE_NAME) #THE chaincode name must be same as the forlder name under chaincode
 
 
 
 echo "========= Installing the newest chaincode ========="
 echo "Chaincode name: $CHAINCODE_NAME"
-peer lifecycle chaincode package "chaincode/$CHAINCODE_NAME/$CHAINCODE_PACKAGE_NAME" -p "chaincode/$CHAINCODE_NAME" -l node
-peer lifecycle chaincode install $CHAINCODE_PACKAGE_NAME
+peer lifecycle chaincode package "chaincode/$CHAINCODE_NAME/$CHAINCODE_PACKAGE_NAME" -p "chaincode/$CHAINCODE_NAME" -l node --label $CHAINCODE_NAME
+peer lifecycle chaincode install "chaincode/$CHAINCODE_NAME/$CHAINCODE_PACKAGE_NAME"
 CC_PACKAGE_ID=$(peer lifecycle chaincode queryinstalled | tail -n 1 |awk '{print $3}' | tr -d ",")
 
 echo "========= Approving chaincode definition ======="
@@ -23,7 +22,7 @@ echo "========= Committing chaincode definition ========="
 peer lifecycle chaincode commit -o orderer:7050 --channelID myc --name $CHAINCODE_NAME --version $VERSION --sequence $SEQUENCE --collections-config "chaincode/$CHAINCODE_NAME/collections_config.json"
 echo "========= Finished Committing chaincode definition ======="
 echo "use peer invoke to invoke transactions:"
-echo "peer chaincode invoke -o orderer:7050 -C myc -n iovcases -c '{\"Args\":[\"init\"]}'"
+echo "peer chaincode invoke -o orderer:7050 -C myc -n $CHAINCODE_NAME -c '{\"Args\":[\"init\"]}'"
 echo "========= CHAINCODE ID ========="
 echo $CC_PACKAGE_ID
 echo "Use chaincode id to start chaincode container"
